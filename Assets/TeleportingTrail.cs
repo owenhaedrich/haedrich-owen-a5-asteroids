fb167fef55063af5193f1ab63240d3a123858281
haedrichowen <owen.haedrich@mohawkcollege.ca>
2025-04-15 13:02:47 -0400

    Add trail to bullets. Reset trail on teleport with TeleportingTrail script.

---
+++ Assets/TeleportingTrail.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TeleportingTrail : MonoBehaviour
+{
+    [SerializeField] private float teleportDistance = 50f;
+
+    TrailRenderer trailRenderer;
+    Vector2 position;
+    Vector2 previousPosition;
+
+    void Start()
+    {
+        trailRenderer = GetComponent<TrailRenderer>();
+        position = transform.position;
+        previousPosition = position;
+    }
+
+    void Update()
+    {
+        previousPosition = position;
+        position = transform.position;
+
+        if (Vector2.Distance(position, previousPosition) > teleportDistance)
+        {
+            trailRenderer.Clear();
+        }
+    }
+}
